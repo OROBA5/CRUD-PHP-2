b7fce9f93f2d1d776a94c458ee866c97e9c80f83
Liga Lomakina <49966055+OROBA5@users.noreply.github.com>
2023-07-17 00:10:11 +0300

    Connection test

--- index.php
+++ index.php
@@ -1,2 +1,28 @@
 <?
+
+include "./config/Database.php";
+include "./connection.php";
+
+
 echo "PHP api for Scandiweb Junior Test";
+
+try {
+    $database = new Database();
+    $conn = $database->getConnection();
+    
+    // If the connection is successful, you can perform a simple query to verify it.
+    $result = $conn->query('SELECT * FROM products');
+    
+    if ($result) {
+        echo 'Database connection successful!';
+    } else {
+        echo 'Database connection failed: ' . $conn->error;
+    }
+    
+    // Don't forget to close the connection when you're done with it.
+    $conn->close();
+} catch (Exception $e) {
+    echo 'Error connecting to the database: ' . $e->getMessage();
+}
+?>
+
